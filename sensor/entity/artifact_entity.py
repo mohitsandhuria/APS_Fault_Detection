47ffb3e0aab39fb6a013a23ebcda50bdacb96159
mohitsandhuria <mohitsandhuria@gmail.com>
2022-11-27 17:05:52 +0530

    Creating folder structure including logger and Exception

---
+++ sensor/entity/artifact_entity.py
@@ -0,0 +1,18 @@
+
+class DataIngestionArtifact:
+    pass
+
+class DataValidationArtifact:
+    pass
+
+class DataTransformationArtifact:
+    pass
+
+class ModelTrainerArtifact:
+    pass
+
+class ModelEvaluationArtifact:
+    pass
+
+class ModelPusherArtifact:
+    pass
