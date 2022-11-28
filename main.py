d0516bbd82ea3fb7f177d59dc4d311fd728df073
mohitsandhuria <mohitsandhuria@gmail.com>
2022-11-28 23:11:10 +0530

    cloading data from mongodb to dataframe

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
 import sys,os
+from sensor.utils import get_collection_as_dataframe
 
-def test_exception_logger():
-     
-     try:
-          logging.info("Starting the try block")
-          result=3/0
-          print(result)
-          logging.info("stoping the try block")
-     except Exception as e:
-          logging.info("Starting the Exception block")
-          raise SensorException(e, sys)
+
+
+
+
 
 if __name__=="__main__":
      try:
-          test_exception_logger()
+          get_collection_as_dataframe(database_name="aps", collection_name="sensor")
      except Exception as e:
           logging.warning(e)
           print(e)
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -5,11 +5,25 @@ from sensor.logger import logging
 import sys,os
 
 
-def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.dataframe:
+def get_collection_as_dataframe(database_name:str,collection_name:str):
+    """
+    Description: This function return collection as dataframe
+    =========================================================
+    Params:
+    database_name: database name
+    collection_name: collection name
+    =========================================================
+    return Pandas dataframe of a collection
+    """
     try:
-        logging.info("Reading Data from Database {} and Collection {}".format(database_name,collection_name))
-        df=pd.DataFrame(list(mongo_client[database_name][collection_name.find()]))
-    
+        logging.info(f"Reading Data from Database {database_name} and Collection {collection_name}")
+        df=pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Found columns: {df.columns}")
+        if "_id" in df.columns:
+            logging.info("Dropping id column from the dataframe")
+            df=df.drop("_id",axis=1)
+        logging.info(f"Rows and columns: {df.shape}")
+        return df
     except Exception as e:
         raise SensorException(e, sys)
 
